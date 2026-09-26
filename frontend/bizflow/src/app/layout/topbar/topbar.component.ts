01df8e5076d31485bdaaed2f2aacb77fd82e1a54
Jonathan Attram <codinghub242@gmail.com>
2026-09-26 17:43:35 +0000

    Core_UI

---
+++ frontend/bizflow/src/app/layout/topbar/topbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-topbar',
+  templateUrl: './topbar.component.html',
+  styleUrls: ['./topbar.component.scss'],
+  imports: [],
+})
+export class TopbarComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
